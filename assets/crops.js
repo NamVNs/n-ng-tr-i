94525db22a9dbc742ab2e13b02036614f3ce3f27
Nam <129677346+NamVNs@users.noreply.github.com>
2023-12-07 20:00:23 +0700

    Create crops.js

---
+++ assets/crops.js
@@ -0,0 +1,78 @@
+module.exports = [
+ {
+ ID: 0,
+ name: "Blue Jazz",
+ levelReqired: 0,
+ Resistance: 1000,
+ season: "Spring",
+ profit: 500,
+ cost: 300,
+ waterCooldown: 1000*60*3,
+ fertilizerCooldown: 1000*60*5,
+ exp: 100,
+ description: "The flower grows in a sphere to invite as many butterflies as possible.",
+ seedImage: 'https://i.imgur.com/IFjIptQ.png',
+ stages: [
+ {
+ duration: 0,
+ image: 'https://i.imgur.com/6hisbpp.png'
+ },
+ {
+ duration: 1000*60*3,
+ image: 'https://i.imgur.com/rXLcS6B.png',
+ },
+ {
+ duration: 1000*60*5,
+ image: 'https://i.imgur.com/aS3Nrjb.png'
+ },
+ {
+ duration: 1000*60*7,
+ image: 'https://i.imgur.com/HpaONo2.png'
+ },
+ {
+     duration: 1000*60*10,
+     image: 'https://i.imgur.com/49PpjBY.png'
+ }
+ ]
+ },
+ {
+ ID: 1,
+ name: "Cauliflower",
+ levelReqired: 0,
+ Resistance: 1000,
+ season: "Spring",
+ profit: 1750,
+ cost: 800,
+ waterCooldown: 1000*60*5,
+ fertilizerCooldown: 1000*60*7,
+ exp: 100,
+ description: "Valuable, but slow-growing. Despite its pale color, the florets are packed with nutrients.",
+ seedImage: 'https://i.imgur.com/N9rLg2a.png',
+ stages: [
+ {
+ duration: 0,
+ image: 'https://i.imgur.com/0YqTdLo.png'
+ },
+ {
+ duration: 1000*60*3,
+ image: 'https://i.imgur.com/GhwzmAY.png',
+ },
+ {
+ duration: 1000*60*7,
+ image: 'https://i.imgur.com/XtD94HM.png'
+ },
+ {
+ duration: 1000*60*11,
+ image: 'https://i.imgur.com/JCtTQ2h.png'
+ },
+ {
+ duration: 1000*60*12,
+ image: 'https://i.imgur.com/i7ttRt0.png'
+ },
+ {
+     duration: 1000*60*15,
+     image: 'https://i.imgur.com/9P3xE8e.png'
+ }
+ ]
+ }
+]
